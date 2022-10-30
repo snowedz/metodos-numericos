9f56a950766ba1822a7af0cfa0cfa09e5adf7762
Brabec <lu_brabec@hotmail.com>
2022-10-29 23:12:51 -0300

    Melhoria de Código e Adição de README

--- metodo.py
+++ metodo.py
@@ -1,14 +1,17 @@
-y0 = 2
-ylinha = 2
-h = 0.1
-x1 = h
-x = []
-y = []
+def main():
+  y0 = 2
+  ylinha = 2
+  h = 0.1
+  x1 = h
+  x = []
+  y = []
 
-for n in range(1,1001):
-  y1 = y0 + h *(x1-y0+ylinha)
-  # print(f'Resultado {n}: {y1}')
-  x1 = h + x1
-  y0 = y1
-  x.append(x1)
-  y.append(y1)
+  for n in range(1,1001):
+    y1 = y0 + h *(x1-y0+ylinha)
+    # print(f'Resultado {n}: {y1}')
+    x1 = h + x1
+    y0 = y1
+    x.append(x1)
+    y.append(y1)
+
+main()
