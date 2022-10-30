a25a1734c5d23d37f2b2c8fc8063c602fd87de0e
Eduardo Neves <62307330+snowedz@users.noreply.github.com>
2022-10-29 22:34:12 -0300

    Create metodo.py

---
+++ metodo.py
@@ -0,0 +1,14 @@
+y0 = 2
+ylinha = 2
+h = 0.1
+x1 = h
+x = []
+y = []
+
+for n in range(1,1001):
+  y1 = y0 + h *(x1-y0+ylinha)
+  # print(f'Resultado {n}: {y1}')
+  x1 = h + x1
+  y0 = y1
+  x.append(x1)
+  y.append(y1)
